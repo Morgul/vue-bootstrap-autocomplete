48b0ba17a8924d1e1b3022d1626e187a16053126
Jeremy House <jer.house@gmail.com>
2023-02-12 22:29:32 -0600

    added files for example

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import VueTypeaheadBootstrap from './components/VueTypeaheadBootstrap.vue'
+import '~bootstrap/scss/bootstrap.scss'
+
+import { reactive } from 'vue'
+
+const state = reactive({query : ''})
+
+</script>
+
+<template>
+
+	<vue-typeahead-bootstrap
+	v-model="state.query" 
+	class="col-md-3"
+	size='sm'
+	placeholder="search"
+    :data="['Canada', 'United States', 'Mexico', 'Netherlands']"
+	/>
+</template>
